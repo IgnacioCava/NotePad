592a4e23f754d67b8675ff6a030e315566e5ff86
IgnacioCava <cavaignacio11@gmail.com>
2021-12-30 11:52:52 -0300

    Styled notes

--- src/Components/editor.jsx
+++ src/Components/editor.jsx
@@ -15,6 +15,22 @@ export default function Editor(){
     const dispatch = useDispatch()
 
     console.log(newNoteContent)
+    console.log(focusedNote.content)
+
+    useEffect(()=>{
+        var editable=document.getElementById('edit')
+        if(editable){
+            if(newNoteContent===focusedNote.content) {
+                editable.disabled=true
+                //editable.style.backgroundColor='#ffffff99'
+                //editable.style.border='1px solid transparent'
+            }
+            else {editable.disabled=false
+                //editable.style.backgroundColor='unset'
+                //editable.style.border='1px solid #ccc'
+            }
+        }
+    })
 
     function Update(){
         Updater(update+1)
@@ -32,15 +48,16 @@ export default function Editor(){
             dispatch(focused(focusedNote.id))
         }}>
 
-            <StyledEditor>
+            <StyledEditor className="inputAreaBg">
                 
-                <InputArea name='InputArea' value={newNoteContent} 
+                <InputArea className='inputArea' name='InputArea' value={newNoteContent} 
                 onChange={(create)=>setNoteContent(create.target.value)}/>
                 
                 <Move>
                     <input type='button' className='input' id='back' onClick={()=>{dispatch(unfocus())}}/>       
                     <input type='submit' className='input' id='edit' value='Editar'/>
-                    <input type='button' className='input' id='erase' value='Borrar' onClick={()=>{
+                    
+                    <input type='button' className='input' id='erase' onClick={()=>{
                         dispatch(erase(focusedNote.id))
                         dispatch(unfocus())
                         }}/>
@@ -48,6 +65,7 @@ export default function Editor(){
 
             </StyledEditor>
         </form>
+        
     )
 }
 
@@ -56,12 +74,23 @@ const Move = styled.div`
     flex-direction: column;
     justify-content: space-around;
     right:0;
-    border-radius: 100%;
+    background-color: transparent;
     > input{
         height: 100%;
         width: 100%;
         border: 1px solid #ccc;
         background-color:#ffffff;
+        transition: .3s;
+    }
+    > #edit{
+        background-color:#ffffff;
+    }
+    > #edit:disabled{
+        background-color:#ffffff99;
+        transition: 1s;
+    }
+    > #edit:active{
+        background-color:lightgreen;
     }
     @media (max-width:800px) {
         flex-direction: row;
@@ -69,16 +98,6 @@ const Move = styled.div`
             height: 40px;
             
         }
-    }
-`
-
-const StyledEditor = styled.div`
-    display: flex;
-    flex-direction: row;
-    width: 100%;
-    height: 100%;
-    @media (max-width:800px) {
-        flex-direction: column;
     }
 `
 
@@ -91,10 +110,11 @@ const InputArea = styled.textarea`
     box-sizing: border-box;
     border: 2px solid #ccc;
     border-right:0px;
-    border-radius: 4px 0 0 4px;
+    border-radius: 10px 0 0 10px;
     outline: none;
     background-color: #ffffff99;
     transition: .3s;
+    
     &:hover{
         background-color: #ffffff;
     }
@@ -105,5 +125,15 @@ const InputArea = styled.textarea`
         border: 2px solid #ccc;
         border-bottom:0px;
         border-radius: 4px 4px 0 0;
+    }
+`
+
+const StyledEditor = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: 100%;
+    @media (max-width:800px) {
+        flex-direction: column;
     }
 `
